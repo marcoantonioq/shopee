69d80625a443be5c6a4da1c0b378f682a8593706
Marco Antônio <marco.aq7@gmail.com>
2025-01-15 22:26:35 -0300

    up

--- src/shopee/products/cupom.js
+++ src/shopee/products/cupom.js
@@ -5,7 +5,7 @@ const cupons = [
     description: '🎟️APLIQUE CUPOM R$ 10 OFF: S5MM3R',
     code: 'S5MM3R',
     rule: (product) =>
-      new Date() < new Date('2025-01-31') && product.price > 80,
+      new Date() < new Date('2025-01-31') && Number(product.price) > 80,
   },
 ]
 
