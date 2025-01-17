8706a88ffb964a77772b537d3919439bee7bf6c9
Marco Antônio <marco.aq7@gmail.com>
2025-01-17 00:29:41 -0300

    up

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import { app } from './infra/express.js'
-import { fetchProductOffers } from './shopee/api/fetchProdctOffers.js'
-import { saveProducts } from './shopee/products/productsSave.js'
 import { state } from './state.js'
 
 const start = async () => {
   app.listen(state.port, () =>
     console.log(`Servidor rodando em http://localhost:${state.port}`)
   )
-
-  const products = await fetchProductOffers()
-
-  try {
-    await saveProducts(products)
-  } catch (error) {
-    console.log('Erro ao salvar: ', error)
-  }
 }
 start()
